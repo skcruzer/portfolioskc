3f1d6c96658a9c8340c5f6671ef28318a79a1214
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-05-24 19:42:15 -0700

    added scripts

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import proPic from '../../assets/pics/small.png'
 import Avatar from '@mui/material/Avatar'
