e3cf2b70d58eed283c8165049f0ff074874f9939
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-05-31 21:25:56 -0700

    projects

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -26,9 +26,7 @@ const ProjectCompCard = ({title, github, heroku, image, description}) => {
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          {description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
